a89367e7d65960a1eaa23a815f8ff0fec40e5d56
Alexandre Amorim <alexandrepinto2005@hotmail.com>
2024-11-03 16:08:46 -0300

    inicio da construção do delete porém sem a implementação da tela

--- db.js
+++ db.js
@@ -15,18 +15,30 @@ async function Connect() {
     return pool.connect();
 }
 
-async function ShowProdutos() {
 
-    const produto = await Connect();
-    console.log("Conexão criada");
 
-    const res = await produto.query("SELECT * FROM produto");
-    console.log("ShowProdutos está on");  
-    produto.release();
+    async function ShowProdutos() {
 
-    return res.rows; 
-    }
+        const produto = await Connect();
+        console.log("Conexão criada");
+    
+        const res = await produto.query("SELECT * FROM produto");
+        console.log("ShowProdutos está on");  
+        produto.release();
+    
+        return res.rows; 
+        }
+    
+        async function DeleteProdutos(id_produto) {
+
+            const produto = await Connect();
+            const query="DELETE FROM produto WHERE id_produto=$1";
+            const values = [id_produto];
+            console.log("Delete ta off");  
+            await produto.query(query,values);
+            }
 
     module.exports = {
-        ShowProdutos      
+        ShowProdutos,
+        DeleteProdutos 
     }
--- index.js
+++ index.js
@@ -13,17 +13,18 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.get("/Gerenciar_Produto", async(req,res) =>{
-
   const produto = await db.ShowProdutos();
   res.render("meusprodutosfazendeiro", { produto });
-  
+})
+
+app.get("/Gerenciar_Produtos/:id", async(req,res) =>{
+  await db.DeleteProdutos(req.params.id);
+  res.sendStatus(200)
 })
 
 app.get("/Adicionar_Produto", async(req,res) =>{
-
     const produto = await db.ShowProdutos();
-    res.render("meusprodutosfazendeiro", { produto });
-    
+    res.render("adicionarproduto", { produto });
   })
 
 
